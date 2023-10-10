a56f925b59e518760f950bbb93c13333403fad0b
Ahmed Al Ansary <ahansaary@gmail.com>
2023-10-10 10:10:11 +0300

    feat(core): add app routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,15 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () =>
+      import('./pages/home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: 'subscription',
+    loadComponent: () =>
+      import('./pages/subscription/subscription.page').then(
+        (m) => m.SubscriptionPage
+      ),
   },
   {
     path: '',
